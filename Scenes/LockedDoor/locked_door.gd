95702455309f0fd5ff94673f2c6d7612129938bf
Francesco Michelini <hello@francescomichelini.com>
2025-04-05 19:26:08 +0200

    Add buttons and doors to dungeon

--- Scenes/LockedDoor/locked_door.gd
+++ Scenes/LockedDoor/locked_door.gd
@@ -1,12 +1,21 @@
 extends StaticBody2D
 
+@export var buttons_needed := 1
+
+@onready var buttons_pressed := 0
 @onready var collision_shape := $CollisionShape2D
 
 func _on_puzzle_button_pressed() -> void:
-  visible = false
-  collision_shape.set_deferred('disabled', true)
+  buttons_pressed += 1
+
+  if buttons_pressed >= buttons_needed:
+    visible = false
+    collision_shape.set_deferred('disabled', true)
 
 
 func _on_puzzle_button_unpressed() -> void:
-  visible = true
-  collision_shape.set_deferred('disabled', false)
+  buttons_pressed -= 1
+
+  if buttons_pressed < buttons_needed:
+    visible = true
+    collision_shape.set_deferred('disabled', false)
