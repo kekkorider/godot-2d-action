efc1499f0a6ee0098243e21633eb3cf88a0773c3
Francesco Michelini <hello@francescomichelini.com>
2025-04-05 19:40:59 +0200

    Add inherited scenes

--- Scenes/PuzzleButton/puzzle_button.gd
+++ Scenes/PuzzleButton/puzzle_button.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
-var bodies_on_top := 0
-
 signal pressed
 signal unpressed
+
+var bodies_on_top := 0
+
+@export var is_single_use := false
 
 @onready var sprite := $AnimatedSprite2D
 
@@ -16,6 +18,9 @@ func _on_body_entered(body: Node2D) -> void:
     pressed.emit()
 
 func _on_body_exited(body: Node2D) -> void:
+  if is_single_use:
+    return
+
   if body.is_in_group('pushable') or body is Player:
     bodies_on_top -= 1
 
