4e4e7c14eaa9090820c26fac0b807901dbacfa9d
Francesco Michelini <hello@francescomichelini.com>
2025-04-06 13:23:40 +0200

    Add switches

---
+++ Scenes/Switch/switch.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+signal switch_activated
+signal switch_deactivated
+
+@onready var sprite := $AnimatedSprite2D
+
+var can_interact := false
+var is_activated := false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed('action') && can_interact:
+		is_activated = !is_activated
+		sprite.play('activated' if is_activated else 'deactivated')
+
+		if is_activated: switch_activated.emit()
+		else: switch_deactivated.emit()
